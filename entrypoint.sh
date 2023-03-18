7a5206a2cc2a29715edd817e8f2ae7f015464602
unknown <dakidhem@gmail.com>
2023-03-18 14:01:38 +0100

    1st_commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+export DJANGO_SUPERUSER_USERNAME=root
+export DJANGO_SUPERUSER_EMAIL=admin@admin.com
+export DJANGO_SUPERUSER_PASSWORD=root
+python manage.py createsuperuser --noinput
+python manage.py runserver 0.0.0.0:8000
+exec "$@"
+
+
+
